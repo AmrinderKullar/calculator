266b13ef111d01a401f4e1657090d05dec059b5a
AmrinderKullar <kullaramrinder@gmail.com>
2021-02-14 21:42:49 +0530

    added theme toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 import CalculatorPage from 'components/calculatorPage';
 
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
+  const toggleTheme = _ => setDarkTheme(!darkTheme);
   return (
-    <div className="App">
+    <div className={`App ${darkTheme ? 'dark' : ''}`}>
+      <div className={'theme-toggle-wrapper'}>
+        <input type="checkbox" name='Dark theme' value={darkTheme} onChange={toggleTheme} />
+        <label>Dark Theme</label>
+      </div>
       <CalculatorPage />
     </div>
   );
