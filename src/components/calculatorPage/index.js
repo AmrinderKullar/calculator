9cfd7acd4e5bf2e9aaf95f96b234bb24406e32d8
AmrinderKullar <kullaramrinder@gmail.com>
2021-02-14 21:42:19 +0530

    added button component

--- src/components/calculatorPage/index.js
+++ src/components/calculatorPage/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'; 
 
 import { numberArr, operatorArr } from 'constants/calculator';
+import Button from 'components/Button';
 
 const CalculatorPage = _ => {
   const [calValue, setCalValue] = useState('');
@@ -9,17 +10,26 @@ const CalculatorPage = _ => {
 
   const updateCalValue = val => {
     setCalValue(`${calValue}${val}`);
-
     if (operator) { setCalValue(`${val}`); setOperator(false); }
   };
 
   const updateRefValue = operator => {
-    setRefValue(`${calValue}${operator}`);
+    setRefValue(`${refValue} ${calValue} ${operator}`);
     setOperator(true);
   };
 
   const getFinalVal = _ => {
-    setCalValue(eval(`${refValue}${calValue}`));
+    let evalString = refValue;
+    if (!refValue) {
+      evalString = calValue;
+    }
+    // case to remove if any operator is added in the end
+    else if (operatorArr.includes(refValue[refValue.length])) {
+      evalString.splice(-1, 1);
+    } else {
+      evalString = `${evalString}${calValue}`;
+    }
+    setCalValue(eval(evalString));
   };
 
   const clearAllInput = _ => {
@@ -27,6 +37,7 @@ const CalculatorPage = _ => {
     setRefValue('');
     setCalValue('');
   };
+
   return (
     <div className={'page-wrapper'}>
       <div className={'input-wrapper'}>
@@ -35,15 +46,15 @@ const CalculatorPage = _ => {
       <div className={'actions-container'}>
         <div className={'actions-wrapper'}>
           <div className={'number-wrapper'}>
-          {numberArr.map(num => <button key={num} onClick={_ => updateCalValue(num)}>{num}</button>)}
+            {numberArr.map(num => <Button key={num.val} onClick={_ => updateCalValue(num.val)} className={num.classname} name={num.val} />)}
           </div>
           <div className={'bottom-operators'}>
-            <button onClick={clearAllInput}>AC</button>
-            <button onClick={getFinalVal}>=</button>
+            <Button onClick={clearAllInput} className={'ac-button'} name={'AC'}/>
+            <Button onClick={getFinalVal} className={'assign-button'} name={'='} />
           </div>
         </div>
         <div className={'operator-wrapper'}>
-          {operatorArr.map(operator => <button key={operator} onClick={_ => updateRefValue(operator)}>{operator}</button>)}
+          {operatorArr.map(operator => <Button key={operator} onClick={_ => updateRefValue(operator)} name={operator} />)}
         </div>
       </div>
     </div>
